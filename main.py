8cc8e6f64c49c3a117affffdd0d41fdb8a87487b
zlqzcc <zlqzcc@gmail.com>
2020-10-22 16:43:16 -0400

    add feedback and rt

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ from prior_learning import *
 
 # create the experiment with n_trial and uniform/non-uniform
 # learning conditions
-experiment = PriorLearningKeyboard(n_trial=10, uniform=True)
+experiment = PriorLearningKeyboard(n_trial=5, uniform=True)
 
 # running the experiment
 experiment.start()
+
+# learning block with natural orientation prior
+experiment.uniform = False
 experiment.run()
+
+experiment.pause()
+
+# learning block with uniform prior
+experiment.uniform = True
+experiment.run()
+
+# end and record the experiment
 experiment.end()
--- prior_learning.py
+++ prior_learning.py
@@ -1,4 +1,4 @@
-from psychopy import core, visual, gui, data, event
+from psychopy import core, visual, gui, data
 import keyboard
 
 import numpy as np 
@@ -24,9 +24,9 @@ class PriorLearning:
         self.inst2 = visual.TextStim(self.win, pos=[0,0], text='After the stimulus, adjust the prob using <-- and --> to match its orientation.')
 
         # initialize stimulus
-        self.target = visual.GratingStim(self.win, sf=0.5, size=10, mask='gauss', contrast=0.1)
-        self.fixation = visual.GratingStim(self.win, color=-1, colorSpace='rgb', tex=None, mask='circle', size=0.2)        
-
+        self.target = visual.GratingStim(self.win, sf=0.5, size=7.5, mask='gauss', contrast=0.10)
+        self.fixation = visual.GratingStim(self.win, color=-1, colorSpace='rgb', tex=None, mask='circle', size=0.2)
+        self.feedback = visual.Line(self.win, start=(0.0, -2.0), end=(0.0, 2.0), lineWidth=5.0, lineColor='white', size=1, contrast=0.75)
         return
 
     def start(self):
@@ -38,22 +38,24 @@ class PriorLearning:
 
         self.io_wait()
 
-        self.stimulus = np.zeros(self.n_trial)
-        self.response = np.zeros(self.n_trial)
+        self.stimulus = []
+        self.response = []
+        self.react_time = []
 
         return
 
     def run(self):
         for idx in range(self.n_trial):
-            # ISI for 1000 ms
+            # ISI for 0.5 s
             self.fixation.draw()
             self.win.flip()
-            core.wait(1.0)
+            core.wait(0.5)
             
             # Draw stimulus for 200 ms
+            # Sample from an orientation distribution (uniform/natural)
             targetOri = sample_orientation(n_sample=1, uniform=self.uniform)
             targetOri = float(targetOri)
-            self.stimulus[idx] = targetOri
+            self.stimulus.append(targetOri)
             
             self.target.setOri(targetOri)
             self.target.draw()
@@ -61,16 +63,37 @@ class PriorLearning:
             self.win.flip()
             core.wait(0.2)
 
-            # blank screen for 1s            
+            # blank screen for 2s            
             self.win.flip()
-            core.wait(1.0)
+            core.wait(2.0)
 
+            # record response
+            clock = core.Clock()
             response = self.io_response()
-            self.response[idx] = response
+
+            self.response.append(response)
+            self.react_time.append(clock.getTime())
+
+            # blank screen for 0.2s            
+            self.win.flip()
+            core.wait(0.2)
+
+            # feedback for 0.5s
+            self.feedback.setOri(response)
+
+            self.target.draw()
+            self.feedback.draw()
+            self.win.flip()
+            core.wait(0.5)
 
     def end(self):
         print(self.stimulus)
         print(self.response)
+        print(self.react_time)
+
+    # No implementaion for pause (during the experiment) for now
+    def pause(self):
+        pass
 
     def io_wait(self):
         raise NotImplementedError("method not implemented in the base class")
