b343292ef95014f722dc20d18d0bb50172a808ae
zlqzcc <zlqzcc@gmail.com>
2020-11-13 17:06:22 -0500

    analysis code

--- analysisMain.m
+++ analysisMain.m
@@ -4,22 +4,23 @@ addpath('./ExpData/');
 
 %% Load Data
 fileName = '10_11_2020_18_04_LQ_0.csv';
-fileName = '11_11_2020_09_08_LQ_0.csv';
+% fileName = '11_11_2020_09_08_LQ_0.csv';
 dataMtx  = readmatrix(fileName);
 
-%% Analysis
+% Analysis
 % moving average bin size
+numBlock = 3;
 binSize = 18;
+blockLength = 200;
 
-figure();
-subplot(1, 3, 1);
-result = analysisBlock(dataMtx, 'blockIndex', 1, 'binSize', binSize);
-scatterPlot(result);
+plotAll(dataMtx, numBlock, blockLength, binSize);
 
-subplot(1, 3, 2);
-result = analysisBlock(dataMtx, 'blockIndex', 2, 'binSize', binSize);
-scatterPlot(result);
-
-subplot(1, 3, 3);
-result = analysisBlock(dataMtx, 'blockIndex', 3, 'binSize', binSize);
-scatterPlot(result);
+%% Helper Function
+function plotAll(dataMtx, numBlock, blockLength, binSize)
+    figure();
+    for idx = 1 : numBlock
+        subplot(1, 3, idx);
+        result = analysisBlock(dataMtx, 'blockIndex', idx, 'blockLength', blockLength, 'binSize', binSize);
+        scatterPlot(result);
+    end
+end
