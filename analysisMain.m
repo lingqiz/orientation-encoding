518cb7f137e41a9b9f3e4734b0100467ab1223f0
zlqzcc <zlqzcc@gmail.com>
2020-11-14 21:37:36 -0500

    add new analysis

--- analysisMain.m
+++ analysisMain.m
@@ -6,11 +6,12 @@ addpath('./ExpData/');
 fileName = '10_11_2020_18_04_LQ_0.csv';
 fileName = '11_11_2020_09_08_LQ_0.csv';
 fileName = '13_11_2020_18_30_LQ_1.csv';
+fileName = '14_11_2020_18_51_taka_0.csv';
 dataMtx  = readmatrix(fileName);
 
 % Analysis
 % moving average bin size
-binSize = 15;
+binSize = 20;
 
 numBlock = 3;
 blockLength = 200;
@@ -19,10 +20,21 @@ plotAll(dataMtx, numBlock, blockLength, binSize);
 
 %% Helper Function
 function plotAll(dataMtx, numBlock, blockLength, binSize)
-    figure();
+    stim = figure();
+    data = figure();
+    % fisher = figure();
     for idx = 1 : numBlock
+        figure(stim);
         subplot(1, numBlock, idx);
+        histogram(dataMtx(1, ((idx - 1) * blockLength + 1) : idx * blockLength), 20);
+        
         result = analysisBlock(dataMtx, 'blockIndex', idx, 'blockLength', blockLength, 'binSize', binSize);
+        figure(data);
+        subplot(1, numBlock, idx);
         scatterPlot(result);
+        
+        figure(fisher);
+        subplot(1, numBlock, idx);
+        fisherPlot(result);
     end
 end
