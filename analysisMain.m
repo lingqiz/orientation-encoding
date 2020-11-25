e8bc9df0cf7d038cf447b15d0bd8883473ec926b
zlqzcc <zlqzcc@gmail.com>
2020-11-25 08:37:04 -0500

    add data

--- analysisMain.m
+++ analysisMain.m
@@ -5,9 +5,10 @@ addpath('./ExpData/');
 %% Load Data
 fileName = {'10_11_2020_18_04_LQ_0.csv', '11_11_2020_09_08_LQ_0.csv', ...
     '13_11_2020_18_30_LQ_1.csv', '14_11_2020_18_51_taka_0.csv'...,
-    '15_11_2020_14_53_LQ_1.csv'};
+    '15_11_2020_14_53_LQ_1.csv', '18_11_2020_18_20_QC_0.csv', ...
+    '19_11_2020_19_18_taka_0.csv', '23_11_2020_17_44_QC_0.csv'};
 
-subId = 1;
+subId = 6;
 dataMtx  = readmatrix(fileName{subId});
 
 % Analysis
