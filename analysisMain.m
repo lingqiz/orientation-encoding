963465e330130d0c265e441322a0caf3789565bc
zlqzcc <zlqzcc@gmail.com>
2021-04-28 09:19:14 -0400

    minor update

--- analysisMain.m
+++ analysisMain.m
@@ -3,7 +3,7 @@ try
     tbUse('plotlab');
     plotlabOBJ = plotlab();
     plotlabOBJ.applyRecipe(...
-        'figureWidthInches', 20, ...
+        'figureWidthInches', 12, ...
         'figureHeightInches', 8);           
 catch EXP
     fprintf('plotlab not available, use default MATLAB style \n');
@@ -16,9 +16,10 @@ addpath('./ExpData/');
 fileName = {'10_11_2020_18_04_LQ_0.csv', '11_11_2020_09_08_LQ_0.csv', ...
     '13_11_2020_18_30_LQ_1.csv', '14_11_2020_18_51_taka_0.csv'...,
     '15_11_2020_14_53_LQ_1.csv', '18_11_2020_18_20_QC_0.csv', ...
-    '19_11_2020_19_18_taka_0.csv', '23_11_2020_17_44_QC_0.csv'};
+    '19_11_2020_19_18_taka_0.csv', '23_11_2020_17_44_QC_0.csv', ....
+    '28_04_2021_07_39_newstim.csv'};
 
-subId = 8;
+subId = 1;
 dataMtx  = readmatrix(fileName{subId});
 
 % Analysis
