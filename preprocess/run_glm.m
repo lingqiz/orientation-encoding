2188198a6afc1f6e5c904702716db09fb0af19ce
lingqiz <zlqzcc@gmail.com>
2022-04-07 13:40:36 -0400

    fit null distribution

--- preprocess/run_glm.m
+++ preprocess/run_glm.m
@@ -15,9 +15,9 @@ for idx = 2:nSes
     % save file name setup (add icafix suffix if applied)
     fl = sprintf('GLM_%s_%s', sub_name, acq_type);
     if icafix
-        fl = strcat(fl, '_ICAFIX');        
+        fl = strcat(fl, '_ICAFIX');
     end
-    fl = strcat(fl, '.mat'); 
+    fl = strcat(fl, '.mat');
     
     % load data defined by ROI
     data = load_session(sub_name, acq_type, acq_idx{idx}, icafix);
@@ -36,3 +36,39 @@ for idx = 2:nSes
     fl_path = fullfile(base_dir, sub_name, fl);
     save(fl_path, 'results', 'roi_mask', 'sub_name', 'acq_type');
 end
+
+%% Run GLM on a "null" distribution
+sub_name = 'HERO_LZ';
+acq_type = 'NeuralCoding01';
+base_dir = '~/Data/fMRI';
+acq_idx = 1:10;
+
+% Load data
+data = load_session(sub_name, acq_type, acq_idx, false);
+roi_mask = define_roi(sub_name);
+
+% Generate Gaussian white noise as "null" data
+dataNull = normrnd(0, 1, size(data(roi_mask, :)));
+
+% load attention event data
+attEvent = load(fullfile(base_dir, sub_name, 'attenRT', 'atten_time.mat'));
+attEvent = attEvent.time;
+
+% Run GLM model fit on null data
+resultsNull = glm_fit(dataNull, attEvent, 0);
+
+%% Run GLM on non-visual area
+maskPath = '~/Desktop/Orientation_Encode/docs/all_visual_areas_mask.nii';
+vareaMask = cifti_read(maskPath);
+vareaMask = vareaMask.cdata;
+
+nonVisual = 1:length(vareaMask);
+nonVisual = nonVisual(~vareaMask);
+
+fprintf('Random select out of %d non-visual voxel \n', length(nonVisual));
+index = randsample(length(nonVisual), sum(roi_mask));
+index = nonVisual(sort(index));
+dataNonVis = data(index, :);
+
+% Run GLM model fit on non-visual voxel
+resultsNonVis = glm_fit(dataNonVis, attEvent, 0);
