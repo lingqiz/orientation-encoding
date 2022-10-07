b542bafa9896ecc9bd86a14c444751bdcd8d4dfb
lingqiz <zlqzcc@gmail.com>
2022-10-07 14:15:52 -0400

    allow for 3SD changes

--- preprocess/glm_fit.m
+++ preprocess/glm_fit.m
@@ -94,7 +94,7 @@ modelOpts = {'polyDeg', 4};
 if strcmp(modelClass, 'mtSinai')
     index = length(modelOpts);
     modelOpts{index + 1} = 'paraSD';
-    modelOpts{index + 2} = 2;
+    modelOpts{index + 2} = 3;
 end
 
 results = forwardModel({data}, {stim}, tr, ...
