5c44a4ceb56054021cdfcaccc83a033ccf49b5c1
lingqiz <zlqzcc@gmail.com>
2022-11-22 15:45:58 -0500

    update default para

--- analysis/svr.py
+++ analysis/svr.py
@@ -6,13 +6,20 @@ class RegressDecode():
         self.range = 180.0
         self.svr = []
 
-    def fit_model(self, stim, resp, epsilon=0.0, lda=1.0):
+        # default parameter for SVR
+        self.epsilon = 0.25
+        self.lda = 7.5e-4
+
+    def fit_model(self, stim, resp):
         stim = stim / self.range * 2 * np.pi
         sin = np.sin(stim)
         cos = np.cos(stim)
 
         for target in [sin, cos]:
-            svr_obj = SVR(kernel='linear', epsilon=epsilon, C=lda)
+            svr_obj = SVR(kernel='linear',
+                        epsilon=self.epsilon,
+                        C=self.lda)
+
             svr_obj.fit(resp.T, target)
             self.svr.append(svr_obj)
 
