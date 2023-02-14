64d01948c5b58ff27b3c873af87abf55cc145c4f
lingqiz <zlqzcc@gmail.com>
2023-02-14 13:31:46 -0500

    add prompt

--- flywheel/hcp_icafix.py
+++ flywheel/hcp_icafix.py
@@ -55,33 +55,34 @@ for sub_label in all_data.keys():
                 submit_icafix(gear, sub_label, 'pRF', analysis_label,
                         prf_ses, data, struct_data, time_stamp, idx)
 
-    # Run gear for NeuralCoding session
-    label = 'NeuralCoding'
-    n_session = 3
-    for idx in range(n_session):
-        ses_label = 'NeuralCoding0%s' % (idx + 1)
-        if ses_label not in all_data[sub_label].keys():
-            print('No %s session found for subject: %s' % (ses_label, sub_label))
-            continue
+    # Run gear for NeuralCoding session if confrimed by the user
+    if get_response('NeuralCoding'):
+        # deprecated: we are not using ICAFIX for orientation stimulus session
+        n_session = 3
+        for idx in range(n_session):
+            ses_label = 'NeuralCoding0%s' % (idx + 1)
+            if ses_label not in all_data[sub_label].keys():
+                print('No %s session found for subject: %s' % (ses_label, sub_label))
+                continue
 
-        stim_ses = all_data[sub_label][ses_label]
-        func_data = []
+            stim_ses = all_data[sub_label][ses_label]
+            func_data = []
 
-        print('\nFinding acquisitions for %s' % ses_label)
-        analyses = stim_ses.analyses
-        for ana in analyses:
-            # get all function runs and list them
-            if ana.label.startswith('hcp-func'):
-                func_data.append(ana)
-                print(ana.label)
+            print('\nFinding acquisitions for %s' % ses_label)
+            analyses = stim_ses.analyses
+            for ana in analyses:
+                # get all function runs and list them
+                if ana.label.startswith('hcp-func'):
+                    func_data.append(ana)
+                    print(ana.label)
 
-        # Run the gear if confrimed by the user
-        if get_response(ses_label):
-            # split the run into two parts
-            split_idx = len(func_data) // 2
-            data_pair = (func_data[:split_idx], func_data[split_idx:])
+            # Run the gear if confrimed by the user
+            if get_response(ses_label):
+                # split the run into two parts
+                split_idx = len(func_data) // 2
+                data_pair = (func_data[:split_idx], func_data[split_idx:])
 
-            # run the gear
-            for idx, data in enumerate(data_pair):
-                submit_icafix(gear, sub_label, ses_label, analysis_label,
-                            stim_ses, data, struct_data, time_stamp, idx)
+                # run the gear
+                for idx, data in enumerate(data_pair):
+                    submit_icafix(gear, sub_label, ses_label, analysis_label,
+                                stim_ses, data, struct_data, time_stamp, idx)
