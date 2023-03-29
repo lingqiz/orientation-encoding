253f9a1f7997db08afe3038219e2d739698ee4b7
lingqiz <zlqzcc@gmail.com>
2023-03-28 21:13:03 -0400

    toggle plotting

--- preprocess/run_avg.m
+++ preprocess/run_avg.m
@@ -1,10 +1,11 @@
 %% Preprocessing with average response within a time window
-sub_name = 'ORNT_TW';
+sub_name = 'ORNT_CMH';
 n_runs = 10;
 n_sessions = 6;
-plot = false;
+cutoff = 180.0;
+plotHRF = false;
 
-if plot
+if plotHRF
     figure();
     set(gcf,'Position',[0 0 1500 1500])
 end
@@ -13,15 +14,15 @@ for idx = 1 : n_sessions
     acq_type = sprintf('Neural%02d', idx);
     fprintf('\nSession %s \n', acq_type);
     
-    % average response
-    avg_resp(sub_name, acq_type, n_runs);
-    
-    % HRF
-    run_idx = randi(n_runs);
-    [tRange, meanSig] = avg_hrf(sub_name, acq_type, run_idx);
-    
-    % plotting
-    if plot
+    % average response    
+    avg_resp(sub_name, acq_type, n_runs, cutoff);
+        
+    if plotHRF
+        % compute HRF
+        run_idx = randi(n_runs);
+        [tRange, meanSig] = avg_hrf(sub_name, acq_type, run_idx);
+
+        % plotting
         subplot(2, 3, idx);    
         plot(tRange, meanSig, '-ok', 'LineWidth', 1.5);
         
