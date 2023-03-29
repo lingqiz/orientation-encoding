881d2f187ba1d853934faf1334958aa9801508b8
lingqiz <zlqzcc@gmail.com>
2023-03-28 20:57:04 -0400

    toggle plotting

--- preprocess/run_avg.m
+++ preprocess/run_avg.m
@@ -2,9 +2,12 @@
 sub_name = 'ORNT_TW';
 n_runs = 10;
 n_sessions = 6;
+plot = false;
 
-figure();
-set(gcf,'Position',[0 0 1500 1500])
+if plot
+    figure();
+    set(gcf,'Position',[0 0 1500 1500])
+end
 
 for idx = 1 : n_sessions    
     acq_type = sprintf('Neural%02d', idx);
@@ -18,14 +21,16 @@ for idx = 1 : n_sessions
     [tRange, meanSig] = avg_hrf(sub_name, acq_type, run_idx);
     
     % plotting
-    subplot(2, 3, idx);    
-    plot(tRange, meanSig, '-ok', 'LineWidth', 1.5);
-    
-    xline(4.0, '--k'); xline(8.0, '--k');
-    ylim([-0.75, 0.75])
-    
-    box off; grid off;
-    xlabel('Time(s)');
-    ylabel('Percent Signal Change');
-    title(sprintf('Session %02d, Run %02d', idx, run_idx));
+    if plot
+        subplot(2, 3, idx);    
+        plot(tRange, meanSig, '-ok', 'LineWidth', 1.5);
+        
+        xline(4.0, '--k'); xline(8.0, '--k');
+        ylim([-0.75, 0.75])
+        
+        box off; grid off;
+        xlabel('Time(s)');
+        ylabel('Percent Signal Change');
+        title(sprintf('Session %02d, Run %02d', idx, run_idx));
+    end
 end
