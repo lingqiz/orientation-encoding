d1926ebe4e97093db5ba02d177fe21c2af97ace6
lingqiz <zlqzcc@gmail.com>
2024-06-20 11:39:06 -0400

    add caching

--- analysis/strpyr.py
+++ analysis/strpyr.py
@@ -11,6 +11,7 @@ class VoxelSimuate():
         self.pyr_idx = pyr_idx
         self.order = order
         self.height = height
+        self.pyr = None
 
         if weight is None:
             self.weight = 1 / (order + 1) * np.ones(order + 1)
@@ -19,22 +20,27 @@ class VoxelSimuate():
 
         self.complex = complex
 
-    def voxel_response(self, image):
+    def build_pyramid(self, image):
         pyr = pt.pyramids.SteerablePyramidFreq(image, order=self.order,
                                                height=self.height,
                                                is_complex=self.complex)
+        self.pyr = pyr
+
+    def voxel_response(self, image):
+        if self.pyr is None:
+            self.build_pyramid(image)
 
         s = self.pyr_idx
-        all_resp = np.zeros((pyr.num_orientations, *pyr.pyr_coeffs[(s, 0)].shape))
-        for c in range(pyr.num_orientations):
+        all_resp = np.zeros((self.pyr.num_orientations, *self.pyr.pyr_coeffs[(s, 0)].shape))
+        for c in range(self.pyr.num_orientations):
             if self.complex:
-                band_re = pyr.pyr_coeffs[(s, c)].real
-                band_im = pyr.pyr_coeffs[(s, c)].imag
+                band_re = self.pyr.pyr_coeffs[(s, c)].real
+                band_im = self.pyr.pyr_coeffs[(s, c)].imag
 
                 # sum of square responses
                 all_resp[c] = np.sqrt(band_re**2 + band_im**2) * self.weight[c]
 
             else:
-                all_resp[c] = np.abs(pyr.pyr_coeffs[(s, c)]) * self.weight[c]
+                all_resp[c] = np.abs(self.pyr.pyr_coeffs[(s, c)]) * self.weight[c]
 
         return np.sum(all_resp, axis=0)
