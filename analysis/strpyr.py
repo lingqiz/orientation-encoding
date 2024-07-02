d698f63c8e07a9373753dde8c2fdf730b2f64745
lingqiz <zlqzcc@gmail.com>
2024-07-02 17:47:45 -0400

    compute pyramid FI

--- analysis/strpyr.py
+++ analysis/strpyr.py
@@ -1,5 +1,6 @@
 import pyrtools as pt
 import numpy as np
+from PIL import Image
 
 class VoxelSimuate():
     '''
@@ -44,3 +45,78 @@ class VoxelSimuate():
                 all_resp[c] = np.abs(self.pyr.pyr_coeffs[(s, c)]) * self.weight[c]
 
         return np.sum(all_resp, axis=0)
+
+
+def voxel_response(image, height='auto'):
+    '''
+    Average response across orientations
+    channels to simulate an "un-tuned" voxel
+    '''
+    pyr = pt.pyramids.SteerablePyramidFreq(image, height=height,
+                                           order=5, is_complex=True)
+
+    # sum orientation responses within each scale
+    response_map = []
+    for s in range(pyr.num_scales):
+
+        all_resp = np.zeros((pyr.num_orientations, *pyr.pyr_coeffs[(s, 0)].shape))
+        for c in range(pyr.num_orientations):
+            band_re = pyr.pyr_coeffs[(s, c)].real
+            band_im = pyr.pyr_coeffs[(s, c)].imag
+
+            # sum of square responses
+            all_resp[c] = np.sqrt(band_re**2 + band_im**2)
+
+        response_map.append(np.mean(all_resp, axis=0))
+
+    return response_map
+
+def all_response(base_path):
+    '''
+    './docs/Stimulus/unornt/' OR
+    './docs/Stimulus/surr_fixed/'
+    '''
+    ornt = np.arange(0, 180, 1)
+    ornt_resp = []
+
+    for o in ornt:
+        stim = Image.open(base_path + f'stim_{o}.png')
+        stim = np.array(stim).astype(float)
+        ornt_resp.append(voxel_response(stim))
+
+    return ornt_resp
+
+def norm_fi(ornt_resp):
+    # Compute FI
+    fi_val = []
+    for idx in range(len(ornt_resp) - 1):
+        delta = ornt_resp[idx + 1] - ornt_resp[idx]
+        fi = np.linalg.norm(delta.flatten())
+        fi_val.append(np.sqrt(fi))
+    fi_val = np.array(fi_val)
+
+    # Normalize FI
+    ornt = np.arange(-90, 90, 1)
+    fi_wrap = np.concatenate([fi_val[-89:], fi_val[:90]])
+    norm_fi = fi_wrap / (np.trapz(fi_wrap, ornt[:-1]) / 180 * 2 * np.pi)
+
+    return ornt[:-1], norm_fi
+
+
+PATH_BASE = './docs/Stimulus/unornt/'
+PATH_SURR = './docs/Stimulus/surr_fixed/'
+
+class PyramidSimulate():
+    def __init__(self, cond):
+        if cond == 'base':
+            self.ornt_resp = all_response(PATH_BASE)
+
+        elif cond == 'surr':
+            self.ornt_resp = all_response(PATH_SURR)
+
+    def set_level(self, level):
+        level_resp = []
+        for r in self.ornt_resp:
+            level_resp.append(r[level])
+
+        self.level_resp = level_resp
